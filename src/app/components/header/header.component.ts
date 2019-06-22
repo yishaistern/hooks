7bbd60142cf7ee6ea723bba5864d3f3ccd37567f
yishaistern <yishainstern@gmail.com>
2019-06-23 01:49:43 +0300

    ds

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,12 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.sass']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  private gonfigArr = [
+    {title: 'a'},
+    {title: 'b'},
+    {title: 'c'},
+    {title: 'd'},
+    {title: 'e'},
+    {title: 'f'},
+    {title: 'g'},
+    {title: 'h'},
+  ];
   constructor() { }
-
+  
   ngOnInit() {
   }
 
