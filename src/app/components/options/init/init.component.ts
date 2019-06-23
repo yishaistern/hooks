a5bf089609b540e9cb0f8253e13735c22b0cf898
yishaistern <yishainstern@gmail.com>
2019-06-24 00:00:22 +0300

    s

--- src/app/components/options/init/init.component.ts
+++ src/app/components/options/init/init.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-init',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InitComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store: Store<{ count: number }>) { }
 
   ngOnInit() {
   }
