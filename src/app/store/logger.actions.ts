8f782a2d0b3b0c91e94151f77f779407a0c24032
yishaistern <yishainstern@gmail.com>
2019-06-23 23:55:34 +0300

    ss

--- src/app/store/logger.actions.ts
+++ src/app/store/logger.actions.ts
@@ -1,16 +1,16 @@
 import { createAction, props } from '@ngrx/store';
 
-export const login = createAction(
-  '[Login Page] Login',
-  props<{ username: string; password: string }>()
-);
+export interface LogItem {
+  console: string; timestamp: number;
+}
+
 
 export const clearLog = createAction(
   '[Log Action] clear'
 );
 
-export const emptyLog = createAction(
-  '[Log Action] empty'
+export const routePage = createAction(
+  '[Log Action] routePage'
 );
 
 export const createLog = createAction(
@@ -21,5 +21,5 @@ export const createLog = createAction(
 
 export const addLog = createAction(
   '[Log Action] add',
-  props<{console: string, timestamp: number}>()
+  props<LogItem>()
 );
--- src/app/store/logger.reducer.ts
+++ src/app/store/logger.reducer.ts
@@ -1,1 +1,35 @@
 import * as logActions from './logger.actions';
+import { Action, createReducer, on } from '@ngrx/store';
+import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
+
+export interface State extends EntityState<logActions.LogItem> {
+    // additional entities state properties
+    selectedLog: string | null;
+}
+
+export function selectLogId(a: logActions.LogItem): number {
+    // In this case this would be optional since primary key is id
+    return a.timestamp;
+}
+
+export function sortByTime(a: logActions.LogItem, b: logActions.LogItem): number {
+    return (a.timestamp + '').localeCompare(b.timestamp + '');
+  }
+
+export const adapter: EntityAdapter<logActions.LogItem> = createEntityAdapter<logActions.LogItem>({
+    selectId: selectLogId,
+    sortComparer: sortByTime,
+  });
+
+export const initialState: State = adapter.getInitialState({
+    // additional entity state properties
+    selectedLog: null,
+ });
+
+const logReducer = createReducer(initialState,
+    on(logActions.clearLog, (state, {}) => {
+        return adapter.removeAll(state);
+    }),
+        on(logActions.addLog, (state, item: logActions.LogItem) => {
+        return adapter.addOne(item, state);
+    }));
