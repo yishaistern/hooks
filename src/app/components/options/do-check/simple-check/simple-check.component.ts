9296d8215c5519067c9f56cd77c9fdf719f70bb8
yishaistern <yishainstern@gmail.com>
2019-06-30 23:13:00 +0300

    do check with differ

--- src/app/components/options/do-check/simple-check/simple-check.component.ts
+++ src/app/components/options/do-check/simple-check/simple-check.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges, SimpleChanges, DoCheck } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, DoCheck, KeyValueDiffers } from '@angular/core';
 import { LogState } from '../../../../store/states';
 import { Store } from '@ngrx/store';
 import * as actions from '../../../../store/logger.actions';
@@ -9,9 +9,13 @@ import * as actions from '../../../../store/logger.actions';
   styleUrls: ['./simple-check.component.scss']
 })
 export class SimpleCheckComponent implements OnInit, DoCheck {
-  
-  constructor(private store: Store<LogState>) {
+  differ: any = {};
+  myCompany = {
+    a: 6
+  };
+  constructor(private store: Store<LogState>, private differs: KeyValueDiffers) {
     console.log('constructor of SimpleCheckComponent');
+    this.differ = this.differs.find(this.myCompany).create();
     this.store.dispatch(actions.createLog({console: 'constructor of SimpleCheckComponent'}));
   }
 
@@ -19,11 +23,23 @@ export class SimpleCheckComponent implements OnInit, DoCheck {
     console.log('init of SimpleCheckComponent');
     this.store.dispatch(actions.createLog({console: 'init of SimpleCheckComponent'}));
     
+    setTimeout(() => {
+      this.myCompany = {
+        a: 5
+      };
+    }, 3000);
+    setTimeout(() => {
+      this.myCompany.a = 18;
+    }, 5000);
   }
 
   ngDoCheck() {
     console.log('DoCheck of SimpleCheckComponent');
-    this.store.dispatch(actions.createLog({console: 'DoCheck of SimpleCheckComponent'}));
+    const customerChanges = this.differ.diff(this.myCompany);
+    if (customerChanges) {
+      console.log(this.myCompany);
+      this.store.dispatch(actions.createLog({console: 'DoCheck of SimpleCheckComponent'}));
+    }
   }
 
 }
