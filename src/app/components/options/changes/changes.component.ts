41adf32d1802ad2221e8f0c3e688c1d33c1e1ae5
yishaistern <yishainstern@gmail.com>
2019-06-30 10:37:06 +0300

    fsdfd

--- src/app/components/options/changes/changes.component.ts
+++ src/app/components/options/changes/changes.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { LogState } from '../../../store/states';
+import { ComponentsState } from '../../../interfaces/interfaces';
+import { CreateComponentsService } from '../../../services/create-components/create-components.service';
+import * as actions from '../../../store/logger.actions';
 
 @Component({
   selector: 'app-changes',
@@ -6,10 +11,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./changes.component.scss']
 })
 export class ChangesComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild('container', {read: ViewContainerRef}) container: ViewContainerRef;
+  constructor(private store: Store<LogState>, private componetCreator: CreateComponentsService) { }
 
   ngOnInit() {
+  }
+
+  createComponent(com: ComponentsState) {
+    this.store.dispatch(actions.newCase({tittle: com.case}));
+    this.componetCreator.newComponent(com.component, this.container);
   }
 
 }
